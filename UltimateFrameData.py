e561f72c0e158c3763e20c8df890cca68eb27dd7
Jakob Haslam <jakehaslam38@gmail.com>
2020-06-25 23:13:53 -0600

    add swift configs

--- UltimateFrameData.py
+++ UltimateFrameData.py
@@ -19,11 +19,11 @@ class UltimateFrameData():
                 if elem.find('a') is not None:
                     my_image = elem.find('a')['data-featherlight']
                 character_data[elem.find('div', class_='movename').get_text().strip()] = {
-                    "active_frames": elem.find('div', class_='activeframes').get_text().strip(),
-                    "on_shield": elem.find('div', class_='advantage').get_text().strip(),
-                    "total_frames": elem.find('div', class_='totalframes').get_text().strip(),
-                    "sheild_lag": elem.find('div', class_='shieldlag').get_text().strip(),
-                    "shield_stun": elem.find('div', class_='shieldstun').get_text().strip(),
+                    "activeFrames": elem.find('div', class_='activeframes').get_text().strip(),
+                    "onShield": elem.find('div', class_='advantage').get_text().strip(),
+                    "totalFrames": elem.find('div', class_='totalframes').get_text().strip(),
+                    "shieldLag": elem.find('div', class_='shieldlag').get_text().strip(),
+                    "shieldStun": elem.find('div', class_='shieldstun').get_text().strip(),
                     "image": my_image
                 }
 
@@ -35,12 +35,12 @@ class UltimateFrameData():
                     my_image = elem.find('a')['data-featherlight']
                 character_data[elem.find('div', class_='movename').get_text().strip()] = {
                     "activeFrames": elem.find('div', class_='activeframes').get_text().strip(),
-                    "on_shield": elem.find('div', class_='advantage').get_text().strip(),
-                    "total_frames": elem.find('div', class_='totalframes').get_text().strip(),
-                    "landing_lag": elem.find('div', class_='landinglag').get_text().strip(),
+                    "onShield": elem.find('div', class_='advantage').get_text().strip(),
+                    "totalFrames": elem.find('div', class_='totalframes').get_text().strip(),
+                    "landingLag": elem.find('div', class_='landinglag').get_text().strip(),
                     "notes": elem.find('div', class_='notes').get_text().strip(),
-                    "sheild_lag": elem.find('div', class_='shieldlag').get_text().strip(),
-                    "shield_stun": elem.find('div', class_='shieldstun').get_text().strip(),
+                    "shieldLag": elem.find('div', class_='shieldlag').get_text().strip(),
+                    "shieldStun": elem.find('div', class_='shieldstun').get_text().strip(),
                     "image": my_image
                 }
 
@@ -52,12 +52,12 @@ class UltimateFrameData():
                     my_image = elem.find('a')['data-featherlight']
                 character_data[elem.find('div', class_='movename').get_text().strip()] = {
                     "activeFrames": elem.find('div', class_='activeframes').get_text().strip(),
-                    "on_shield": elem.find('div', class_='advantage').get_text().strip(),
-                    "total_frames": elem.find('div', class_='totalframes').get_text().strip(),
-                    "landing_lag": elem.find('div', class_='landinglag').get_text().strip(),
+                    "onShield": elem.find('div', class_='advantage').get_text().strip(),
+                    "totalFrames": elem.find('div', class_='totalframes').get_text().strip(),
+                    "landingLag": elem.find('div', class_='landinglag').get_text().strip(),
                     "notes": elem.find('div', class_='notes').get_text().strip(),
-                    "sheild_lag": elem.find('div', class_='shieldlag').get_text().strip(),
-                    "shield_stun": elem.find('div', class_='shieldstun').get_text().strip(),
+                    "shieldLag": elem.find('div', class_='shieldlag').get_text().strip(),
+                    "shieldStun": elem.find('div', class_='shieldstun').get_text().strip(),
                     "image": my_image
                 }
 
@@ -70,7 +70,7 @@ class UltimateFrameData():
                 # might need to revisit to get active frames instead of startup frames 
                 character_data[elem.find('div', class_='movename').get_text().strip()] = {
                     "startup": elem.find('div', class_='startup').get_text().strip(),
-                    "total_frames": elem.find('div', class_='totalframes').get_text().strip(),
+                    "totalFrames": elem.find('div', class_='totalframes').get_text().strip(),
                     "notes": elem.find('div', class_='notes').get_text().strip(),
                     "image": my_image
                 }
@@ -82,8 +82,8 @@ class UltimateFrameData():
                 if elem.find('a') is not None:
                     my_image = elem.find('a')['data-featherlight']
                     character_data[elem.find('div', class_='movename').get_text().strip()] = {
-                    "total_frames": elem.find('div', class_='totalframes').get_text().strip(),
-                    "landing_lag": elem.find('div', class_='landinglag').get_text().strip(),
+                    "totalFrames": elem.find('div', class_='totalframes').get_text().strip(),
+                    "landingLag": elem.find('div', class_='landinglag').get_text().strip(),
                     "notes": elem.find('div', class_='notes').get_text().strip(),
                     "image": my_image
                 }
